2ecde519a276b389079516ffcb0f1f9148072b6e
Shafique Rehman <alisasoli20@gmail.com>
2019-07-17 11:37:14 +0500

    First Commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+
+class UserController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = Auth::user();
+
+        if ($user->isAdmin()) {
+            return view('pages.admin.myhome');
+        }
+
+        return view('pages.user.home');
+    }
+    
+
+    
+}
